bf80640e32bc8eeef562b2c440a7f18a4bd5c98d
Eldarik25 <Fot1380123@mail.ru>
2024-04-07 13:42:00 +0300

    4

--- github/ViewController.swift
+++ github/ViewController.swift
@@ -9,20 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        //Ruslan
-        
-        
-        
-        // Do aetbvgrebrgnrtegvfsany additional setup after loading the view.
-        
-        // eldar lev
-        
-        
-        
         
     }
 
